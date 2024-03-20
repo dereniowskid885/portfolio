aa426fffb14d6a2156d64fcb4f7432909aaf92df
Daniel Dereniowski <leinad885@o2.pl>
2024-03-20 20:19:47 +0100

    env variables fix for conflict with sanity studio, sanity client install, aliases defined

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,13 +1,13 @@
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-03-17';
 
 export const dataset = assertValue(
-    process.env.SANITY_STUDIO_DATASET,
-    'Missing environment variable: SANITY_STUDIO_DATASET'
+    process.env.SANITY_STUDIO_DATASET || process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET,
+    'Missing environment variable: DATASET'
 );
 
 export const projectId = assertValue(
-    process.env.SANITY_STUDIO_PROJECT_ID,
-    'Missing environment variable: SANITY_STUDIO_PROJECT_ID'
+    process.env.SANITY_STUDIO_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID,
+    'Missing environment variable: PROJECT_ID'
 );
 
 export const useCdn = false;
